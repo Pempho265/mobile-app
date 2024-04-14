cd7752663ce9539f7cedcf2b2c2f3a0ef8f7df70
pempho <pemphochibaya@gmail.com>
2024-04-14 22:50:09 +0200

    ecoherb

---
+++ app/src/main/java/com/example/ecoherb/welcome1.java
@@ -0,0 +1,32 @@
+package com.example.ecoherb;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class welcome1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void welcome2(View v){
+        Intent intent = new Intent(welcome1.this, welcome2.class);
+        startActivity(intent);
+
+    }
+}
---
+++ app/src/main/java/com/example/ecoherb/welcome2.java
@@ -0,0 +1,35 @@
+package com.example.ecoherb;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+
+public class welcome2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_welcome2);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void Back_fuction(View v){
+        Intent intent = new Intent(welcome2.this, welcome1.class);
+        startActivity(intent);
+    }
+    public void Next_fuction(View v){
+        Intent intent = new Intent(welcome2.this, recent.class);
+        startActivity(intent);
+    }
+}
